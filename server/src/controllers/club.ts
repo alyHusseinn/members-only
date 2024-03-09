d74eb52a34f350dc98ab2aa9a3118f3acc63a9e0
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-03-09 21:56:05 +0200

    adding the club controller methods

--- server/src/controllers/club.ts
+++ server/src/controllers/club.ts
@@ -1,24 +1,83 @@
-import Club from '../models/club';
 import { Request, Response, NextFunction } from 'express';
+import { asyncHandler } from 'middlewares/asyncHandler';
+import Club from '../models/club';
+import { body, validationResult } from 'express-validator';
 
-const getAllClubs = (req: Request, res: Response, next: NextFunction) => {
-  //
-};
+const getAllClubs = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const clubs = await Club.find({}).select('title description').exec();
+  res.json(clubs);
+});
 
-const getClub = (req: Request, res: Response, next: NextFunction) => {
-  //
-};
+const getClub = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const club = await Club.findById(req.params.id, '-password').populate('posts').exec();
+  if (!club) {
+    res.json({ error: 'Club not found' });
+  }
+  res.json(club);
+});
 
-const create_club = (req: Request, res: Response, next: NextFunction) => {
-  //
-};
+const create_club = [
+  body('title').trim().escape().isLength({ min: 5 }).withMessage('Title Should Be More than 5 characters long!'),
+  body('description')
+    .trim()
+    .escape()
+    .isLength({ min: 30 })
+    .withMessage('Description Should Be More than 30 characters long!'),
+  body('passwords').trim().isLength({ min: 8 }).withMessage('Passwords Should Be More than 8 characters long!'),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ errros: errors.array() });
+    } else {
+      const newClub = new Club({
+        title: req.body.title,
+        description: req.body.description,
+        password: req.body.password
+        // admin: req.user.id
+      });
 
-const update_club = (req: Request, res: Response, next: NextFunction) => {
-  //
-};
+      await newClub
+        .save()
+        .then(() => res.status(200).end())
+        .catch(() => res.status(400).send({ error: 'This Name already exists' }));
+    }
+  })
+];
 
-const delete_club = (req: Request, res: Response, next: NextFunction) => {
-  //
-};
+const update_club = [
+  body('title').trim().escape().isLength({ min: 5 }).withMessage('Title Should Be More than 5 characters long!'),
+  body('description')
+    .trim()
+    .escape()
+    .isLength({ min: 30 })
+    .withMessage('Description Should Be More than 30 characters long!'),
+  body('passwords').trim().isLength({ min: 8 }).withMessage('Passwords Should Be More than 8 characters long!'),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ errros: errors.array() });
+    } else {
+      const club = new Club({
+        title: req.body.title,
+        description: req.body.description,
+        password: req.body.password,
+        _id: req.body.id
+        // admin: req.user.id
+      });
+
+      await Club.findByIdAndUpdate(req.body.id, club, {}).exec();
+    }
+  })
+];
+
+const delete_club = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const club = await Club.findById(req.params.id).exec();
+  if (!club) {
+    const error = new Error('This Club does not exist');
+    next(error);
+  } else {
+    await Club.findByIdAndDelete(req.params.id).exec();
+  }
+});
 
 export { getAllClubs, getClub, create_club, update_club, delete_club };
--- server/src/models/club.ts
+++ server/src/models/club.ts
@@ -2,20 +2,44 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const ClubSchema = new Schema({
+export interface ClubDocument extends mongoose.Document {
+  title: string;
+  password: string;
+  description?: string;
+  admin: mongoose.Types.ObjectId;
+  subscribers?: mongoose.Types.ObjectId[];
+  posts?: mongoose.Types.ObjectId[];
+  getAllClubs(): Promise<ClubDocument[]>;
+}
+
+const ClubSchema = new Schema<ClubDocument>({
   title: {
     type: String,
-    required: true
-    // unique: [true, 'This Name is already Exist!']
+    required: true,
+    unique: true
+    //unique: [true, 'This Name is already Exist!']
   },
-  descreption: {
+  password: {
+    type: String,
+    required: true
+  },
+  description: {
     type: String
   },
   admin: {
-    type: mongoose.Types.ObjectId,
-    ref: 'user'
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
   },
-  subscribers: [{ type: mongoose.Types.ObjectId, ref: 'user', required: true }]
+  subscribers: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  posts: [{ type: Schema.Types.ObjectId, ref: 'post' }]
 });
 
-export default mongoose.model('club', ClubSchema);
+ClubSchema.statics.getAllClubs = async function (): Promise<ClubDocument[]> {
+  return this.find({}, 'title description').exec();
+};
+
+// Define model
+const Club: mongoose.Model<ClubDocument> = mongoose.model<ClubDocument>('club', ClubSchema);
+
+export default Club;
